6d00379afc974dc5f259eff417a43ca2f97227c3
zc <z304307393@163.com>
2018-11-10 18:16:43 +0800

    first

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
+import Loadable from 'react-loadable'
+
+const One = Loadable({ loader: () => import('./views/One.js'), loading: () => null });
+const Two = Loadable({ loader: () => import('./views/Two.js'), loading: () => null });
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route path="/" exact component={One} />
+          <Route path="/two" component={Two} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default App;
